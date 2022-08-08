b244dbdf2fb91130069580c8b4a78c280571c195
jiale-olver-zhang <jy00297602@gmail.com>
2022-08-08 23:38:25 +0800

    update

---
+++ scripts/python/document_your_node/main.py
@@ -0,0 +1,27 @@
+from PySide2 import QtGui,QtCore,QtWidgets,QtUiTools
+import hou
+import os
+
+ui_file_main = os.path.join(os.path.dirname(__file__),'ui',"document_your_node_ui.ui")
+class MainWidgets(QtWidgets.QWidget):
+    
+    def __init__(self,parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        
+        # load ui file
+        loader = QtUiTools.QUiLoader()
+        self.ui = loader.load(ui_file_main,parentWidget=self)
+
+        self.setParent(hou.qt.mainWindow(), QtCore.Qt.Window)
+
+    def closeEvent(self, event):
+        self.setParent(None)
+    
+
+def show():
+    dialog = MainWidgets()
+    dialog.show()
+    return dialog
+
+if __name__ == "__main__":
+    show()
